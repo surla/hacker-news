e1d597f7829e34c7e6c015c91abfcc15fdff0c57
Joseph Surla <josephsurla@gmail.com>
2017-02-11 18:53:08 -0800

    Set initial internal component state

--- src/App.js
+++ src/App.js
@@ -21,17 +21,34 @@ const list = [
 ];
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: list,
+    };
+  }
+
   render() {
     return (
       <div className="App">
-          { list.map(item =>
+          { this.state.list.map(item =>
               <div key={item.objectID}>
                 <span>
-                  <a href="{item.url}">{item.title}</a>
+                  <a href={item.url}>{item.title}</a>
                 </span>
                 <span>{item.author}</span>
                 <span>{item.num_comments}</span>
                 <span>{item.points}</span>
+                <span>
+                  <button
+                    onClick={() => this.onDismiss(item.objectID)}
+                    type="button"
+                  >
+                    Dismiss
+                  </button>
+                </span>
               </div>
             )}
       </div>
