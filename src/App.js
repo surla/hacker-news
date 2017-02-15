cf3d7a8a6c3e129274c769dfc6f7f8ab02ca92d7
Joseph Surla <josephsurla@gmail.com>
2017-02-14 22:22:12 -0800

    Refactor render and add const for searchTerm and list

--- src/App.js
+++ src/App.js
@@ -48,6 +48,7 @@ class App extends Component {
     }
 
   render() {
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
           <form>
@@ -56,7 +57,7 @@ class App extends Component {
               onChange={this.onSearchChange}
               />
           </form>
-           { this.state.list.filter(isSearched(this.state.searchTerm)).map(item =>
+           { list.filter(isSearched(this.state.searchTerm)).map(item =>
               <div key={item.objectID}>
                 <span>
                   <a href={item.url}>{item.title}</a>
